ee0e7f39b408327b6d46cacd5db0803f6a7abcae
simon19f3 <simonassfaw@gmail.com>
2026-04-21 17:42:31 +0300

    feat(users): add query keys factory for users cache management

---
+++ src/features/user/constants/userKeys.ts
@@ -0,0 +1,10 @@
+// features/users/constants/userKeys.ts
+
+export const userKeys = {
+  all: ['users'] as const,
+  lists: () => [...userKeys.all, 'list'] as const,
+  list: () => [...userKeys.lists()] as const,
+  details: () => [...userKeys.all, 'detail'] as const,
+  detail: (id: string) =>
+    [...userKeys.details(), id] as const,
+};
